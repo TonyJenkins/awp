86360fc99c199fbc5a7974e543e8b2185f65d883
Tony Jenkins <tony.jenkins@elder-studios.co.uk>
2020-02-06 12:11:50 +0000

    Protect routes to require authentication

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,23 @@
 <?php
 
-Route::get ('/', 'CommentController@index');
+Route::group (['middleware' => 'auth'], function () {
+    Route ::get ('/', 'CommentController@index');
 
-Route::get ('/comment/{comment}/', 'CommentController@show');
+    Route ::get ('/comment/{comment}/', 'CommentController@show');
 
-Route::get ('/comment/{comment}/delete/', 'CommentController@destroy');
-
-
-Route::get ('/comment/{comment}/edit/', 'CommentController@edit');
-Route::post ('/comment/{comment}/edit/', 'CommentController@update');
-
-Route::get ('/add/', 'CommentController@create');
-Route::post ('/add/', 'CommentController@store');
+    Route ::get ('/comment/{comment}/delete/', 'CommentController@destroy');
 
 
-Route::get ('/comment/{comment}/like/', 'LikesController@upVote');
-Route::get ('/comment/{comment}/dislike/', 'LikesController@downVote');
+    Route ::get ('/comment/{comment}/edit/', 'CommentController@edit');
+    Route ::post ('/comment/{comment}/edit/', 'CommentController@update');
+
+    Route ::get ('/add/', 'CommentController@create');
+    Route ::post ('/add/', 'CommentController@store');
+
+
+    Route ::get ('/comment/{comment}/like/', 'LikesController@upVote');
+    Route ::get ('/comment/{comment}/dislike/', 'LikesController@downVote');
+});
 
 Auth::routes();
 
